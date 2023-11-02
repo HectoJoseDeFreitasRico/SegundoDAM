1d525cd7acef713cee0f9798d7c44dd3eb38e08b
HectoJoseDeFreitasRico <hjfnogueira@gmail.com>
2023-11-02 22:32:00 +0100

    aquí mi proyecto

---
+++ Programación multimedia y dispositivos móviles/Semana 3/videojuegos/js/Personaje.js
@@ -0,0 +1,84 @@
+/*Creamos una plantilla .para un personaje*/
+class Personaje{
+	/*Dotamos al personaje de una serie de propiedades iniciales*/
+	constructor(x,y,z,direccion,direccionisometrica,color,tiemponacimiento,estadoanim,energia){
+		/*Le proporciono unos valores de inicio a la instancia*/
+		this.tiemponacimiento = 0;
+		this.x = Math.random()*(terrenox2-terrenox1)+terrenox1;
+		this.y = Math.random()*(terrenoy2-terrenoy1)+terrenoy1;
+		this.direccion = Math.PI*2*Math.random();
+		this.direccionverdadera = Math.PI*2*Math.random();
+		this.direccionisometrica = Math.floor(Math.random()*4);
+		this.color = "blue";
+		this.estadoanim = Math.floor(Math.random()*7);
+		this.energia = 100;
+	}
+	/*Creamos un metodo para gestionar el movimiento del personaje*/
+	mueve(){
+		/*El personaje va envejeciendo*/
+		this.tiemponacimiento += 1;
+		if(this.tiemponacimiento % 100 == 0){
+			this.cambiadireccion();
+		}
+		
+		/*Actualizamos el angulo en radianes segun la nomenglatura de angulos de 0 a 3*/
+		if(this.direccionisometrica == 0){
+			this.direccion = 0;
+		}else if(this.direccionisometrica == 1){
+			this.direccion = Math.PI/2;
+		}else if(this.direccionisometrica == 2){
+			this.direccion = Math.PI;
+		}else if(this.direccionisometrica == 3){
+			this.direccion = Math.PI*1.5;
+		}
+		
+		/*Esto actualiza la posicion del personaje*/
+		this.x -= Math.cos(this.direccion);
+		this.y -= Math.sin(this.direccion);
+		
+		/*Va cambiando de estado de animacion para caminar*/
+		this.estadoanim++;
+		if(this.estadoanim > 7){this.estadoanim = 0;}
+		
+		/*Cada 100s del personaje pierde energia*/
+		if(this.tiemponacimiento % 100 == 0){this.pierdeenergia();}
+		/*Calcula las colisiones*/
+		this.colisiona();
+	}
+	
+	persigue(){
+		this.estadoanim++;
+		if(this.estadoanim > 7){this.estadoanim = 0;}
+		
+		this.x -= (this.x - posx)/100;
+		this.y -= (this.y - posy)/100;
+		
+		this.x -= Math.cos(this.direccion);
+		this.y -= Math.sin(this.direccion);
+		
+	}
+	
+	/*Aqui le indicamos al programa que el personaje cambia de direccion cada 100 U de vida*/
+	cambiadireccion(){
+		if(this.tiemponacimiento % 100 == 0){this.direccionisometrica = Math.floor(Math.random()*4);}
+	}
+
+	colisiona(){
+		if(
+			this.x > terrenox2 
+			|| 
+			this.x < terrenox1
+			|| 
+			this.y > terrenoy2 
+			|| 
+			this.y < terrenoy1){
+				if(this.direccionisometrica == 1){this.direccionisometrica = 3;}
+				else if(this.direccionisometrica == 3){this.direccionisometrica = 1;}
+				else if(this.direccionisometrica == 0){this.direccionisometrica = 2;}
+				else if(this.direccionisometrica == 2){this.direccionisometrica = 0;}
+		}
+	}
+	pierdeenergia(){
+		this.energia -= 1;
+	}
+}
---
+++ Programación multimedia y dispositivos móviles/Semana 3/videojuegos/js/condicionesiniciales.js
@@ -0,0 +1,45 @@
+/* Capturamos el lienzo y lo introduciomos dentro de una variable contexto */
+var contexto = document.getElementById("lienzo").getContext("2d");
+/*Aqui voy a declarar las variables globales a todo el programa*/
+var temporizador;
+var anchuranavegador = window.innerWidth;
+var alturanavegador = window.innerHeight;
+document.getElementById("lienzo").height = alturanavegador;
+document.getElementById("lienzo").width = anchuranavegador;
+document.getElementById("fondo").height = alturanavegador;
+document.getElementById("fondo").width = anchuranavegador;
+document.getElementById("contenedor").height = alturanavegador;
+document.getElementById("contenedor").width = anchuranavegador;
+/*En esta zona vamos a cargar las imagenes que vamos a utilizar en el videojuego*/
+var imagennpc1 = new Image();
+imagennpc1.src = "img/personajerojo.png";
+var imagenpersonaje = new Image();
+imagenpersonaje.src = "img/personajeverde.png";
+var imagenpremio = new Image();
+imagenpremio.src = "img/premio.png";
+
+/*Introduzco la lista de los personajes*/
+var numeropersonajes = 5;
+var arraypersonajes = new Array();
+
+//PROPIEDADES PROTAGONISTAS
+var posx = 1000;
+var posy = 200;
+var estadoanimacion = 0;
+var angulo = 0;
+var velocidad = 6;
+var direccion = 0;
+var energia = 100;
+/*Aqui introducimos las condiciones minimas y maximas del terreno en el cual se moveran los npcs*/
+var terrenox1 = 600;
+var terrenoy1 = -300;
+var terrenox2 = 1400;
+var terrenoy2 = 500;
+
+var premiox = 800;
+var premioy = 400;
+
+var nivel = 1;
+
+var pausa = false;
+
