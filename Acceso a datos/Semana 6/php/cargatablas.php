4b68f1ddc7590f999a454d3bf0b4e9af0f21aea4
HectoJoseDeFreitasRico <hjfnogueira@gmail.com>
2023-11-12 13:18:37 +0100

    aquí mi proyecto

---
+++ Acceso a datos/Semana 6/php/cargatablas.php
@@ -0,0 +1,13 @@
+<?php
+include "config.php";
+
+$consulta = "SHOW TABLES";
+
+$resultado = $mysqli->query($consulta);
+
+    
+    while ($row = $resultado->fetch_array()) {
+        echo '<option value="'.$row[0].'">'.$row[0].'</option>';
+    }
+
+?>
