c0badbc0aabb0528c51ed89898132735ab788163
= <hjfnogueira@gmail.com>
2024-02-04 21:40:18 +0100

    mi proyecto

---
+++ Sistemas de Gestión Empresarial/Semana 13/piedrapapelotijera.py
@@ -0,0 +1,38 @@
+import random
+
+print("Piedra, papel o tijera, v0.02")
+def bucle():
+    print("1 = piedra ")
+    print("2 = papel ")
+    print("3 = tijera ")
+    tirada = int(input("escoge tu tirada: "))
+
+    tiradaordenador = random.randint(1,3)
+
+    if tiradaordenador == 1:
+        print("El ordenador a sacado piedra")
+    elif tiradaordenador == 2:
+        print("El ordenador a saca papel")
+    elif tiradaordenador == 3:
+        print("El ordenador ha sacado tijera")
+
+    if tirada == 1 and tiradaordenador == 1:
+        print("Empate")
+    elif tirada == 3 and tiradaordenador == 3:
+        print("Empate")
+    elif tirada == 2 and tiradaordenador == 2:
+        print("Empate")
+    elif tirada == 1 and tiradaordenador == 2:
+        print("Pierde el jugador")
+    elif tirada == 2 and tiradaordenador == 3:
+        print("Pierde el jugador")
+    elif tirada == 3 and tiradaordenador == 1:
+        print("Pierde el jugador")
+    elif tirada == 1 and tiradaordenador == 3:
+         print("Jugador gana")
+    elif tirada == 2 and tiradaordenador == 1:
+         print("Jugador gana")
+    elif tirada == 3 and tiradaordenador == 2:
+        print("Jugador gana")
+    bucle()
+bucle()
